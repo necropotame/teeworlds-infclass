9121815738f633e3b11f1b908bdb80eaaaa7c618
necropotame <necropotame@gmail.com>
2015-12-10 20:34:58 +0100

    better translation system

---
+++ src/game/server/text/german.h
@@ -0,0 +1,112 @@
+	"%s entered and joined the game",
+	"%s changed his/her name to %s",
+	"%s left the game",
+	"%s has been kicked (%s)",
+	"%s has been banned (%s)",
+	
+	"Du bist für %i Sekunden eingefroren",
+    "Du hast %s infiziert, +3 Punkte",
+    "Du hast eine Hexe getötet, +5 Punkte",
+    "Du hast überlebt, +5 Punkte",
+    "Du hast %s mit einem Portal infiziert, -5 Punkte",
+    "Du hast %s mit einem Portal getötet, -5 Punkte",
+    "%s wurde infiziert",
+    "Die Hexe ist da !",
+    "Die Hexe ist tot",
+    "Der Untote ist da !",
+    "Der Untote ist nun endgültig tot",
+   
+    "Die Infizierten haben die Runde in %i:%02i Minuten gewonnen",
+    "Ein Überlebender hat die Runde gewonnen",
+    "%i Überlebende haben die Runde gewonnen",
+   
+    "Wähle deine Klasse indem du auf die entsprechende Waffe klickst",
+    "Zufällige Auswahl",
+   
+    "Engineer",
+    "Soldier",
+    "Scientist",
+    "Medic",
+    "Ninja",
+    "Smoker",
+    "Boomer",
+    "Hunter",
+    "Untoter",
+    "Hexe",
+   
+    "Du bist folgender Überlebender: Engineer",
+    "Du bist folgender Überlebender: Soldier",
+    "Du bist folgender Überlebender: Scientist",
+    "Du bist folgender Überlebender: Medic",
+    "Du bist folgender Überlebender: Ninja",
+    "Du bist folgender Infizierter: Smoker",
+    "Du bist folgender Infizierter: Boomer",
+    "Du bist folgender Infizierter: Hunter",
+    "Du bist folgender Infizierter: Untoter",
+    "Du bist folgender Infizierter: Hexe",
+   
+    "Gib \"/help Engineer\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Soldier\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Scientist\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Medic\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Ninja\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Smoker\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Boomer\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Hunter\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Undead\" ein für zusätzliche Informationen zu deiner Klasse",
+    "Gib \"/help Witch\" ein für zusätzliche Informationen zu deiner Klasse",
+   
+    "Liste der Befehle\n\n"
+        "/info: Informationen zu diesem Mod\n\n"
+        "/help: Regeln zu diesem Spiel\n\n"
+        "/help <Klasse>: Informationen zu einer bestimmten Klasse\n\n"
+        "/language <lang>: Ändert die Sprache des Mods\n\n"
+        "/customskin <none|me|all>: Macht den eigenen Skin für niemanden, nur für mich oder alle Überlebenden sichtbar\n\n"
+        "/alwaysrandom <0|1>: Wählt automatisch eine zufällige Klasse wenn die Runde beginnt\n\n"
+        "Drücke <F3> oder <F4> während du <TAB> gedrückt hast, um eine andere Anzeige im Score zu erhalten",
+    "InfectionClass, von necropotame (version 0.5)\n"
+        "Basiert auf den Infection mod von Gravity\n"
+        "Dank an guenstig werben, Defeater, Orangus and BlinderHeld",
+    "Regeln zum Spiel:\n\n"
+        "InfectionClass ist ein Teamspiel. Überlebende gegen Infizierte\n\n"
+        "Alle Spieler starten als Überlebende.\n"
+        "Nach 10 Sekunden werden zwei Überlebende zu Infizierten.\n\n"
+        "Das Ziel der Überlebenden ist es zu überleben bis die Armee eingreift.\n\n"
+        "Das Ziel der Infizierten ist es, alle Überlebenden zu verseuchen",
+    "Engineer:\n\n"
+        "Der Ingenieur kann Walls erzeugen, die die Infizierten abblocken.\n"
+        "Wenn ein Infizierter an die Wall kommt, stirbt er.\n\n"
+        "Die Lebensdauer einer Wall beträgt 30 Sekunden. Außerdem kann nur eine Wall pro Ingenieur aktiv sein.",
+    "Soldier:\n\n"
+        "Der Soldat kann schwebende Bomben setzen.\n"
+        "Jede Bombe kann er drei mal explodieren lassen.\n"
+        "Benutze den Hammer um die Bomben zu setzen und explodieren zu lassen.\n\n"
+        "Es kann nur eine Bombe pro Soldat gesetzt sein.",
+    "Scientist:\n\n",
+    "Medic:\n\n"
+        "Der Medic kann Überlebende durch schlagen mit dem Hammer Schilde geben.\n\n"
+        "Er hat eine starke Schrotflinte, mit der er Infizierte weit zurückschießen kann.\n",
+    "Ninja:\n\n"
+        "Der Ninja kann Granaten werfen, die Infizierte für 3 Sekunden einfrieren.\n\n"
+        "Sein Hammer ist durch eine Katana ersetzt, die ihm erlaubt zwei Mal mir ihr durch die Luft zu schlagen, bevor der Spieler wieder den Boden berühren muss.\n",
+    "Smoker :\n\n"
+        "Der Smoker kann mit seinem Hammer entweder Infizierte heilen oder Überlebende verseuchen.\n\n"
+        "Außerdem kann er Überlebenden 4 Schadenspunkte pro Sekunde zufügen, in denen er sie hookt.",
+    "Boomer :\n\n"
+        "Der Boomer explodiert, wenn er angreift.\n"
+        "Alle Überlebenden, die von der Explosion etwas abbekommen, werden verseucht.\n\n"
+        "Außerdem kann er Überlebenden 1 Schadenspunkt pro Sekunde zufügen, in denen er sie hookt.",
+    "Hunter :\n\n"
+        "Der Hunter kann mit seinem Hammer entweder Infizierte heilen oder Überlebende verseuchen.\n\n"
+        "Er kann zwei mal in der Luft springen.\n\n"
+        "Außerdem kann er Überlebenden 1 Schadenspunkt pro Sekunde zufügen, in denen er sie hookt.",
+    "Untoter :\n\n"
+        "Der Untote kann mit seinem Hammer entweder Infizierte heilen oder Überlebende verseuchen.\n\n"
+        "Er wird für 10 Sekunden eingefroren, anstatt zu sterben.\n"
+        "Wenn er von einem Infizierten geheilt wird, so wird der Einfriereffekt sofort wieder aufgehoben.\n\n"
+        "Außerdem kann er Überlebenden 1 Schadenspunkt pro Sekunde zufügen, in denen er sie hookt.",
+    "Hexe :\n\n"
+        "Die Hexe kann mit ihrem Hammer entweder Infizierte heilen oder Überlebende verseuchen.\n\n"
+        "Wenn ein Infizierter stirbt, kann er neben ihr wiederbelebt werden.\n"
+        "Wenn die Hexe stirbt, verschwindet sie und wird durch eine andere Klasse der Infizierten ersetzt.\n\n"
+        "Außerdem kann sie Überlebenden 1 Schadenspunkt pro Sekunde zufügen, in denen sie sie hookt."
