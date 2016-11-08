33e5a214b1353665f1ff871cf09db1f47bc7248e
necropotame <necropotame@gmail.com>
2016-11-08 09:22:04 +0100

    Mercenary bombs

--- src/game/server/entities/merc-bomb.cpp
+++ src/game/server/entities/merc-bomb.cpp
@@ -20,8 +20,31 @@ void CMercenaryBomb::Reset()
 
 void CMercenaryBomb::Explode()
 {
+	float InnerRadius = 48.0f;
+	
 	GameServer()->CreateSound(m_Pos, SOUND_GRENADE_EXPLODE);
-	GameServer()->CreateExplosion(m_Pos, m_Owner, WEAPON_HAMMER, false, TAKEDAMAGEMODE_NOINFECTION);
+	
+	for(int i=0; i<8; i++)
+	{
+		float Angle = 2.0f * pi * static_cast<float>(i)/8.0f;
+		GameServer()->CreateExplosion(m_Pos + vec2(cos(Angle), sin(Angle)) * InnerRadius, m_Owner, WEAPON_HAMMER, true);
+	}
+	
+	CCharacter *apEnts[MAX_CLIENTS];
+	float Radius = 135.0f;
+	int Num = GameServer()->m_World.FindEntities(m_Pos, Radius, (CEntity**)apEnts, MAX_CLIENTS, CGameWorld::ENTTYPE_CHARACTER);
+	for(int i = 0; i < Num; i++)
+	{
+		vec2 Diff = apEnts[i]->m_Pos - m_Pos;
+		vec2 ForceDir(0,1);
+		float l = length(Diff);
+		if(l)
+			ForceDir = normalize(Diff);
+		l = 1-clamp((l-InnerRadius)/(Radius-InnerRadius), 0.0f, 1.0f);
+		float Dmg = 12 * l;
+		if((int)Dmg)
+			apEnts[i]->TakeDamage(ForceDir*Dmg*2, (int)Dmg, m_Owner, WEAPON_HAMMER, TAKEDAMAGEMODE_NOINFECTION);
+	}
 	
 	GameServer()->m_World.DestroyEntity(this);
 }
