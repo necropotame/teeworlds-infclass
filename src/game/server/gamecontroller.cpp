2a11e1282eb57419975be6fb866702f884e83136
Valentin Lorentz <progval+github@progval.net>
2017-05-21 12:25:34 +0200

    Do not kick players when they are the only (non-spectating) player. (#198)

--- src/game/server/gamecontroller.cpp
+++ src/game/server/gamecontroller.cpp
@@ -450,8 +450,15 @@ void IGameController::Tick()
 		m_UnbalancedTick = -1;
 	}
 
+	unsigned int nbPlayers=0;
+	CPlayerIterator<PLAYERITER_INGAME> Iter(GameServer()->m_apPlayers);
+	while(Iter.Next())
+	{
+		nbPlayers++;
+	}
+
 	// check for inactive players
-	if(g_Config.m_SvInactiveKickTime > 0)
+	if(g_Config.m_SvInactiveKickTime > 0 && nbPlayers > 1)
 	{
 		for(int i = 0; i < MAX_CLIENTS; ++i)
 		{
