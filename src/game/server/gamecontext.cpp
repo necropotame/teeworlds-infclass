68c68e10fd21b309acf166a1b552bdb3877b3338
yavl <yavl98@mail.ru>
2018-09-26 19:54:38 +0900

    small fix

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -126,7 +126,7 @@ int CGameContext::GetZombieCount() {
 	for(int i = 0; i < MAX_CLIENTS; i++)
 	{
 		if (!m_apPlayers[i])
-			break;
+			continue;
 		if (m_apPlayers[i]->IsInfected())
 			count++;
 	}
