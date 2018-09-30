71b9104d7d0cc773316fd3a983818453b8c4ac70
yavl <yavl98@mail.ru>
2018-10-01 02:30:05 +0900

    Unfreeze zombie after revive

--- src/game/server/entities/laser.cpp
+++ src/game/server/entities/laser.cpp
@@ -48,6 +48,7 @@ bool CLaser::HitCharacter(vec2 From, vec2 To)
 		else {
 			zombie->GetPlayer()->SetClass(old_class);
 			zombie->GetPlayer()->GetCharacter()->SetHealthArmor(1, 0);
+			zombie->Unfreeze();
 			medic->TakeDamage(vec2(0.f, 0.f), 38, m_Owner, WEAPON_RIFLE, TAKEDAMAGEMODE_NOINFECTION);
 		}
 	}
