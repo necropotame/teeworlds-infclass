4e9cac6af5312f5ed7544e2d53d79d90132046af
yavl <yavl98@mail.ru>
2018-10-08 18:33:24 +0900

    Removed infc_sewers & infc_provence (boring, may come back after changes)

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -4185,14 +4185,13 @@ void CServer::OnRoundEnd()
 		str_comp(m_aCurrentMap, "infc_lunaroutpost") != 0 &&
 		str_comp(m_aCurrentMap, "infc_skull") != 0 &&
 		str_comp(m_aCurrentMap, "infc_warehouse") != 0 &&
-		str_comp(m_aCurrentMap, "infc_sewers") != 0 &&
 		str_comp(m_aCurrentMap, "infc_damascus") != 0 &&
 		str_comp(m_aCurrentMap, "infc_eidalfitr") != 0 &&
-		str_comp(m_aCurrentMap, "infc_provence") != 0 &&
 		str_comp(m_aCurrentMap, "infc_newdust") != 0 &&
 		str_comp(m_aCurrentMap, "infc_normandie") != 0 &&
 		str_comp(m_aCurrentMap, "infc_junglewell") != 0 &&
-		str_comp(m_aCurrentMap, "infc_cherrytemple") != 0
+		str_comp(m_aCurrentMap, "infc_cherrytemple") != 0 &&
+		str_comp(m_aCurrentMap, "infc_deathdealer") != 0
 	)
 		return;
 	
