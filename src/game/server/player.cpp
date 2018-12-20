dc1a78ae308c0a141f62996c5aa40325f2e8e978
yavl <yavl98@mail.ru>
2018-12-20 21:49:51 +0900

    Fix player getting infected several times in a row

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -509,6 +509,8 @@ void CPlayer::SetTeam(int Team, bool DoChatMsg)
 		{
 			GameServer()->SendChatTarget_Localization(-1, CHATCATEGORY_PLAYER, _("{str:PlayerName} joined the spectators"), "PlayerName", Server()->ClientName(m_ClientID), NULL);
 			GameServer()->AddSpectatorCID(m_ClientID);
+			SetClass(PLAYERCLASS_NONE);
+			Server()->InfecteClient(m_ClientID);
 		}
 		else
 		{
