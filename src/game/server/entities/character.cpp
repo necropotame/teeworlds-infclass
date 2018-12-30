1ad32915ddd995ce3efd445b461a44d7b7f4b163
Julien Midedji <Julien.Midedji@gmail.com>
2018-12-30 23:53:22 +0100

    Format

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -2741,25 +2741,27 @@ void CCharacter::Snap(int SnappingClient)
 				pObj->m_Type = WEAPON_HAMMER;
 			}
 		}
-		else if(GetClass() == PLAYERCLASS_HERO) {
-		CHeroFlag *pFlag = (CHeroFlag*) GameWorld()->FindFirst(CGameWorld::ENTTYPE_HERO_FLAG);
-		
-		// Guide hero to flag
-		if(pFlag->GetCoolDown() <= 0) {
-			CNetObj_Projectile *pObj = static_cast<CNetObj_Projectile *>(Server()->SnapNewItem(NETOBJTYPE_PROJECTILE, m_CursorID, sizeof(CNetObj_Projectile)));
-			if(!pObj)
-				return;
+		else if(GetClass() == PLAYERCLASS_HERO) 
+		{
+			CHeroFlag *pFlag = (CHeroFlag*) GameWorld()->FindFirst(CGameWorld::ENTTYPE_HERO_FLAG);
+			
+			// Guide hero to flag
+			if(pFlag->GetCoolDown() <= 0)
+			{
+				CNetObj_Projectile *pObj = static_cast<CNetObj_Projectile *>(Server()->SnapNewItem(NETOBJTYPE_PROJECTILE, m_CursorID, sizeof(CNetObj_Projectile)));
+				if(!pObj)
+					return;
 
-			float Angle = atan2f(pFlag->m_Pos.y-m_Pos.y, pFlag->m_Pos.x-m_Pos.x);
-			vec2 Indicator = m_Pos + vec2(cos(Angle), sin(Angle)) * 84.0f; 
+				float Angle = atan2f(pFlag->m_Pos.y-m_Pos.y, pFlag->m_Pos.x-m_Pos.x);
+				vec2 Indicator = m_Pos + vec2(cos(Angle), sin(Angle)) * 84.0f; 
 
-			pObj->m_X = (int)Indicator.x;
-			pObj->m_Y = (int)Indicator.y;
-			pObj->m_VelX = 0;
-			pObj->m_VelY = 0;
-			pObj->m_StartTick = Server()->Tick();
+				pObj->m_X = (int)Indicator.x;
+				pObj->m_Y = (int)Indicator.y;
+				pObj->m_VelX = 0;
+				pObj->m_VelY = 0;
+				pObj->m_StartTick = Server()->Tick();
+			}
 		}
-	}
 	}
 /* INFECTION MODIFICATION END ***************************************/
 
