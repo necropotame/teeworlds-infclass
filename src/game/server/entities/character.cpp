3ef420b4369a8bc1d33e3ddb4bdd2a3d06ca8626
duralakun <duralakun@gmail.com>
2019-01-15 10:16:18 +0100

    small condition fix

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -2747,7 +2747,8 @@ bool CCharacter::TakeDamage(vec2 Force, int Dmg, int From, int Weapon, int Mode)
 		else
 		{
 			//If the player is a new infected, don't infected other -> nobody knows that he is infected.
-			if(pKillerPlayer && !pKillerPlayer->IsInfected() || (Server()->Tick() - pKillerPlayer->m_InfectionTick)*Server()->TickSpeed() < 0.5) return false;
+			if (pKillerPlayer)
+				if(!pKillerPlayer->IsInfected() || (Server()->Tick() - pKillerPlayer->m_InfectionTick)*Server()->TickSpeed() < 0.5) return false;
 		}
 	}
 
